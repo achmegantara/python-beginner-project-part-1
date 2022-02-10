3193c8387abe8bf17886444afcd2ef16e38d2786
Achmad Akbar Megantara <achmegantara@gmail.com>
2022-02-10 17:19:36 +0700

    finish madlib

---
+++ madilibs/madlibs.py
@@ -0,0 +1,10 @@
+# String concatenation
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+name_person = input("Name : ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because i love to {verb1}. Stay hydrated and {verb2} like you are {name_person}!"
+
+print(madlib)
